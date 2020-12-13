bff161a8fc81e8de92d472e99874b490c2bc91fb
Rahul Kumawat <rahulkkumawat2002@gmail.com>
2020-12-13 11:13:43 +0530

    Day 4 Code Added

---
+++ 05 - Flex Panel Gallery/index.js
@@ -0,0 +1,13 @@
+const panels = document.querySelectorAll(".panel");
+
+function addOpen(){
+   this.classList.toggle("open");
+}
+
+function addOpenActive(e){
+    if(e.propertyName.includes("flex"))
+        this.classList.toggle("open-active");
+}
+
+panels.forEach(panel => panel.addEventListener("click",addOpen));
+panels.forEach(panel => panel.addEventListener("transitionend",addOpenActive));
